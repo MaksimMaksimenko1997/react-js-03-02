c2e3e10fcc61b1c0030f2bb585592bc0593878bf
MaksimMaksimenko1997 <59928413+MaksimMaksimenko1997@users.noreply.github.com>
2020-09-13 11:37:21 +0300

    Updates

---
+++ precache-manifest.6ce29e31dd9c79b55ed299071069fca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223933c2ef38e56d3b68e454d8a622ad",
+    "url": "/react-js-03-02/index.html"
+  },
+  {
+    "revision": "0afcab6bf2cba62d84cd",
+    "url": "/react-js-03-02/static/css/main.60ddaa74.chunk.css"
+  },
+  {
+    "revision": "4f343881486c351798c3",
+    "url": "/react-js-03-02/static/js/2.cddb6d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-03-02/static/js/2.cddb6d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afcab6bf2cba62d84cd",
+    "url": "/react-js-03-02/static/js/main.4c83fa17.chunk.js"
+  },
+  {
+    "revision": "d844c295d4bba591d097",
+    "url": "/react-js-03-02/static/js/runtime-main.00b54db3.js"
+  }
+]);
